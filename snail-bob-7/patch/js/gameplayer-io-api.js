fb4ddce56fe19b96608a2f5af14bb6b38fff17e8
Haleyschool <taikhoanlenvoi@gmail.com>
2024-05-26 04:36:45 +0700

    61

--- snail-bob-7/patch/js/gameplayer-io-api.js
+++ snail-bob-7/patch/js/gameplayer-io-api.js
@@ -1,39 +1,40 @@
-console.l0g= console.log;
-consolel0g= console.log;
+console.l0g = console.log;
+consolel0g = console.log;
 
-GameAPI= {
-  "loadAPI": function() {
-  },
-  "GameBreak": {
-    "request": function(fn1, fn2) {
+GameAPI = {
+  loadAPI: function () {},
+  GameBreak: {
+    request: function (fn1, fn2) {
       fn1();
-      fn2();        
-    }
-  },
-  "Branding": {
-    "getLink": function(args) {
-      return {
-        "action": function() {
-          consolel0g("--fx--GameAPI--Branding--getLink--action--");
-          window.open("https://ads.games235.com/"+ (args?"?q="+args:""));
-        }
-      }      
+      fn2();
     },
-    "getLogo": function(){
+  },
+  Branding: {
+    getLink: function (args) {
       return {
-        "image": "patch/images/null.png",
-        "action": function() {
+        action: function () {
+          consolel0g("--fx--GameAPI--Branding--getLink--action--");
+          window.open(
+            "https://haleyschool.github.io/" + (args ? "?q=" + args : "")
+          );
+        },
+      };
+    },
+    getLogo: function () {
+      return {
+        image: "patch/images/null.png",
+        action: function () {
           consolel0g("--fx--GameAPI--Branding--getLogo--action--");
-          window.open("https://ads.games235.com/");
-        }
-      }
-    }
-  }
-}
+          window.open("https://haleyschool.github.io/");
+        },
+      };
+    },
+  },
+};
 
-AudioMixer= {
-  "isWebAudioSupport": function() {
+AudioMixer = {
+  isWebAudioSupport: function () {
     consolel0g("--fx--AudioMixer--isWebAudioSupport--");
     return false;
-  }  
-}
+  },
+};
